cfab3c918454935a0575f893e0f49681360ea704
Nashad <nashad@pc-21.home>
2021-02-12 04:16:44 +0100

    fix front + add controlle register

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        return $this->render('/security/register.html.twig');
+    }
+
+}
